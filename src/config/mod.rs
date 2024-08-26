5395397bbf3d546f1e63a0fe51a6f3715313a2ac
DanielSerenoTN <daniel.marin@tendril.us>
2024-08-25 20:04:00 -0600

    feat: centralization of global configurations

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,10 +1,15 @@
-use mongodb::{Client, options::ClientOptions};
 use std::env;
 
-pub async fn init_mongo_client() -> mongodb::error::Result<Client> {
-    let mongo_uri = env::var("MONGO_URI").expect("MONGO_URI must be set");
-    let mut client_options = ClientOptions::parse(&mongo_uri).await?;
-    client_options.app_name = Some("Saar API".to_string());
-    let client = Client::with_options(client_options)?;
-    Ok(client)
+pub struct GlobalConfig {
+    pub addrs: String,
+    pub app_name: String,
+}
+
+impl GlobalConfig {
+    pub fn from_env() -> Self {
+        Self {
+            addrs: env::var("ADDRS").unwrap_or_else(|_| "127.0.0.1:8080".into()),
+            app_name: env::var("APP_NAME").unwrap_or_else(|_| "saar_api".into()),
+        }
+    }
 }
